03f265c91ec02f857b6e27143dd4c2140cd2abe6
aki <lmxox@proton.me>
2026-05-18 00:33:25 +0900

    refactor: Overall code reorganization

--- flake.nix
+++ flake.nix
@@ -19,62 +19,57 @@
     }:
     let
       system = "x86_64-linux";
+
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs { inherit system overlays; };
 
       rustToolchain = pkgs.rust-bin.stable."1.86.0".default.override {
         targets = [ "wasm32-wasip1" ];
       };
+
       rustPlatform = pkgs.makeRustPlatform {
         cargo = rustToolchain;
         rustc = rustToolchain;
       };
+
       wasmedgeArchive = pkgs.fetchurl {
         url = "https://github.com/WasmEdge/WasmEdge/releases/download/0.14.1/WasmEdge-0.14.1-fmt-patch-debian11_x86_64_static.tar.gz";
         sha256 = "sha256-CuSU8OJR1/4bB+QhoZic4qnyvzKEHOMIbfwI7xjJ/Pg=";
       };
 
-      containerd-shim-wasmtime-v1 = rustPlatform.buildRustPackage rec {
+      containerd-shim-wasmtime-v1 = rustPlatform.buildRustPackage {
         pname = "runwasi";
         version = "0-unstable-2026-03-02";
-        doCheck = false;
         src = runwasi-src;
-
         cargoHash = "sha256-lsiCdfxQ0BpavoV1ar0yXPa/16AgDl31umoBCguPowE=";
 
+        doCheck = false;
         buildAndTestSubdir = ".";
-
         cargoBuildFlags = [
           "--bin"
           "containerd-shim-wasmtime-v1"
         ];
 
-        nativeBuildInputs = with pkgs; [
-          protobuf
-          pkg-config
-          cmake
-          gnumake
-          gcc
+        nativeBuildInputs = [
+          pkgs.protobuf
+          pkgs.pkg-config
+          pkgs.cmake
         ];
 
         CMAKE_POLICY_VERSION_MINIMUM = 3.5;
-
-        LIBCLANG_PATH = pkgs.lib.makeLibraryPath (
-          with pkgs;
-          [
-            libclang
-          ]
-        );
+        LIBCLANG_PATH = pkgs.lib.makeLibraryPath ([
+          pkgs.libclang
+        ]);
         WASMEDGE_STANDALONE_ARCHIVE = "${wasmedgeArchive}";
 
-        buildInputs = with pkgs; [
-          systemd
-          dbus-glib
-          libelf
-          libseccomp
-          libclang
-          zstd
-          openssl_3
+        buildInputs = [
+          pkgs.systemd
+          pkgs.dbus-glib
+          pkgs.libelf
+          pkgs.libseccomp
+          pkgs.libclang
+          pkgs.zstd
+          pkgs.openssl_3
         ];
 
       };
